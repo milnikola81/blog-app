dd117d8749904dbafda76f1af5d445850c160529
Nikola <milnikola81@gmail.com>
2018-08-16 19:21:41 +0200

    finished no.1

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <nav class="navbar navbar-dark bg-dark">
+            <router-link to="/posts">Posts</router-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Navbar from './components/Navbar.vue';
+import AppPosts from './components/AppPosts.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {path: '/', redirect: '/posts'},
+  {path: '/posts', component: AppPosts}
+  // {path: '/contacts/:id', component: Contacts, name: 'contact-details'}
+];
+
+const router = new VueRouter({
+  routes
+});
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App), router
 }).$mount('#app')
